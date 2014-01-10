51abb52b36c05dcb52c9a9ce6d54498ecdbcd17a
typeofb <typeofb@gmail.com>
2014-01-10 13:34:38 +0900

    com.ibatis.common.jdbc.SimpleDataSource -> org.apache.commons.dbcp.BasicDataSource

--- src/com/daumit/daummng/dao/DaumMngDao.java
+++ src/com/daumit/daummng/dao/DaumMngDao.java
@@ -2,8 +2,12 @@ package com.daumit.daummng.dao;
 
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.web.db.SqlConfig;
@@ -14,6 +18,11 @@ public class DaumMngDao {
 	private SqlMapClient sql = SqlConfig.getSqlMapInstance();
 
 	public List<Map<String, Object>> selectBBS(Map<String, Object> map) {
+		DataSource dataSource = sql.getDataSource();
+		BasicDataSource basicDataSource = (BasicDataSource) dataSource;
+		System.out.println("Active 상태 객체수: " + basicDataSource.getNumActive());
+		System.out.println("Idle 상태 객체수: " + basicDataSource.getNumIdle());
+
 		List<Map<String, Object>> result = null;
 		try {
 			result = sql.queryForList("DaumMng.selectBBS", map);
