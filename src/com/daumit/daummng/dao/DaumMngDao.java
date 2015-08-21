5aadee7431d5d83b82578b7af13b0ef6f2ec9e6f
unknown <typeofb@gmail.com>
2015-08-21 10:59:51 +0900

    jdbc 예제

--- src/com/daumit/daummng/dao/DaumMngDao.java
+++ src/com/daumit/daummng/dao/DaumMngDao.java
@@ -1,12 +1,14 @@
 package com.daumit.daummng.dao;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.web.db.SqlConfig;
@@ -15,6 +17,7 @@ import com.web.db.SqlConfig;
 public class DaumMngDao {
 	
 	private SqlMapClient sql = null;
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	
 	public DaumMngDao() {
 		sql = SqlConfig.getSqlMapInstance();
@@ -22,6 +25,8 @@ public class DaumMngDao {
 		BasicDataSource basicDataSource = (BasicDataSource) dataSource;
 		System.out.println("Active 상태 객체수: " + basicDataSource.getNumActive());
 		System.out.println("Idle 상태 객체수: " + basicDataSource.getNumIdle());
+		
+		this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
 	}
 	
 	public List<Integer> selectCopy(List<Integer> list) {
@@ -37,7 +42,11 @@ public class DaumMngDao {
 	public List<Map<String, Object>> selectBBS() {
 		List<Map<String, Object>> result = null;
 		try {
-			result = sql.queryForList("DaumMng.selectBBS");
+//			result = sql.queryForList("DaumMng.selectBBS");
+			String sql = "SELECT BOARD_ID, BOARD_NAME FROM BOARD WHERE BOARD_NAME = :BOARD_NAME";
+			Map<String, Object> paramMap = new HashMap<String, Object>();
+			paramMap.put("BOARD_NAME", "NOTICE");
+			result = namedParameterJdbcTemplate.queryForList(sql, paramMap);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
