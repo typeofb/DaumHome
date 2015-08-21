6fb50c7979137604920aa4e98c3f79e5ee25d6ec
unknown <typeofb@gmail.com>
2015-08-21 09:32:32 +0900

    extends SqlConfig

--- src/com/daumit/sysmng/dao/BoardMngDao.java
+++ src/com/daumit/sysmng/dao/BoardMngDao.java
@@ -3,42 +3,35 @@ package com.daumit.sysmng.dao;
 import java.util.HashMap;
 import java.util.List;
 
-import com.ibatis.sqlmap.client.SqlMapClient;
 import com.web.db.SqlConfig;
 
 @SuppressWarnings("unchecked")
-public class BoardMngDao {
-	
-	private SqlMapClient sql = null;
-	
-	public BoardMngDao() {
-		sql = SqlConfig.getSqlMapInstance();
-	}
+public class BoardMngDao extends SqlConfig {
 	
 	public List<HashMap<String, Object>> selectBoardList(HashMap<String, Object> iMaps) {
 		List<HashMap<String, Object>> result = null;
 		try {
-			result = sql.queryForList("BoardMng.selectBoardList", iMaps);
+			result = getSqlMapInstance().queryForList("BoardMng.selectBoardList", iMaps);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
-
+	
 	public int selectBoardCnt(HashMap<String, Object> iMaps) {
 		int result = 0;
 		try {
-			result = (Integer) sql.queryForObject("BoardMng.selectBoardCnt", iMaps);
+			result = (Integer) getSqlMapInstance().queryForObject("BoardMng.selectBoardCnt", iMaps);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
-
+	
 	public HashMap<String, Object> selectBoardDetail(String postId) {
 		HashMap<String, Object> result = null;
 		try {
-			result = (HashMap<String, Object>) sql.queryForObject("BoardMng.selectBoardDetail", postId);
+			result = (HashMap<String, Object>) getSqlMapInstance().queryForObject("BoardMng.selectBoardDetail", postId);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -48,7 +41,7 @@ public class BoardMngDao {
 	public boolean insertBoard(HashMap<String, Object> iMaps) {
 		boolean result = false;
 		try {
-			sql.update("BoardMng.insertBoard", iMaps);
+			getSqlMapInstance().update("BoardMng.insertBoard", iMaps);
 			result = true;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -59,18 +52,18 @@ public class BoardMngDao {
 	public boolean updateBoard(HashMap<String, Object> iMaps) {
 		boolean result = false;
 		try {
-			sql.update("BoardMng.updateBoard", iMaps);
+			getSqlMapInstance().update("BoardMng.updateBoard", iMaps);
 			result = true;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
-
+	
 	public boolean deleteBoard(String postId) {
 		boolean result = false;
 		try {
-			sql.delete("BoardMng.deleteBoard", postId);
+			getSqlMapInstance().delete("BoardMng.deleteBoard", postId);
 			result = true;
 		} catch (Exception e) {
 			e.printStackTrace();
