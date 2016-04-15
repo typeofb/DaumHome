4352fdc5e7177596b3002fce43e5da79b098496b
Shin <typeofb@gmail.com>
2016-04-15 12:42:43 +0900

    DI return page to main

--- src/com/daumit/daummng/controller/DiController.java
+++ src/com/daumit/daummng/controller/DiController.java
@@ -12,19 +12,21 @@ public class DiController implements Controller {
 
 	// 다형성을 이용(X)
 	private ICommonDao dao; // = new OracleDaoImpl();
-
+	
 	// 스프링이 생성한 객체를 주입받아야 하므로 setter 메서드가 필요하다.
 	public void setDao(ICommonDao dao) {
 		this.dao = dao;
 	}
-
+	
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
 		dao.select("메시지");
 		dao.insert("메시지");
 		dao.update("메시지");
 		dao.delete("메시지");
-
-		return null;
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("main");
+		return mav;
 	}
 }
